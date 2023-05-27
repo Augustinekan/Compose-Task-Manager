68e95322684c7db56b083db4ea0046678530f352
Augustinekan <augustinekanyuira0@gmail.com>
2023-05-27 21:56:48 +0300

    display and align image

--- app/src/main/java/com/example/composetaskmanager/MainActivity.kt
+++ app/src/main/java/com/example/composetaskmanager/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.composetaskmanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composetaskmanager.ui.theme.ComposeTaskManagerTheme
 
@@ -22,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                   TaskDoneColumn()
                 }
             }
         }
@@ -30,14 +35,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TaskDoneColumn(){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment =Alignment.CenterHorizontally
+    ) {
+        TaskCompleteCard()
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ComposeTaskManagerTheme {
-        Greeting("Android")
-    }
+fun TaskCompleteCard(){
+    val imageResource = painterResource(id = R.drawable.ic_task_completed);
+    Image(painter = imageResource, contentDescription = "Task Done Image")
 }
