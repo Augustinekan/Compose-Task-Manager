b6f1eee542d649b8e3f48e2f2b20edf4e0840047
Augustinekan <augustinekanyuira0@gmail.com>
2023-05-27 22:05:42 +0300

    add title and subtitle text

--- app/src/main/java/com/example/composetaskmanager/MainActivity.kt
+++ app/src/main/java/com/example/composetaskmanager/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -14,7 +15,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composetaskmanager.ui.theme.ComposeTaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,18 +38,32 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview(showBackground = true)
 @Composable
 fun TaskDoneColumn(){
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment =Alignment.CenterHorizontally
     ) {
-        TaskCompleteCard()
+        TaskCompleteImageCard()
+        TitleText()
+        SubTitleText()
     }
 }
 
 @Composable
-fun TaskCompleteCard(){
-    val imageResource = painterResource(id = R.drawable.ic_task_completed);
+fun TaskCompleteImageCard(){
+    val imageResource = painterResource(id = R.drawable.ic_task_completed)
     Image(painter = imageResource, contentDescription = "Task Done Image")
+}
+
+
+@Composable
+fun TitleText(modifier: Modifier = Modifier){
+    Text(text = "All tasks completed",  fontWeight = FontWeight.Bold, modifier = modifier.padding(top = 24.dp, bottom = 8.dp))
+}
+
+@Composable
+fun SubTitleText(modifier: Modifier = Modifier){
+    Text(text = "Nice work!", fontSize = 16.sp,modifier=modifier)
 }
